00cfaeab58557e89fcbd6eb7eda8d8567d5710f6
ScytheMarshall <172561988+ScytheMarshall@users.noreply.github.com>
2025-07-21 22:26:21 -0400

    Adding -kit:egg
    There are many ways to scramble an egg. This kit gives you one such
    way, for your starting character. Turns out, there are a *lot* of
    special cases to handle, especially thanks to funny things like
    Push B to Jump and various wacky flags. This flag probably needs
    some real-gameplay testing.
    
    The only file changed (aside from flagspec/etc., which happened
    in a previous commit) is kit_rando.py.

--- FreeEnt/kit_rando.py
+++ FreeEnt/kit_rando.py
@@ -1,6 +1,6 @@
 from . import databases
 import re
-
+from .rewards import RewardSlot
 
 KIT_SPECS = {
     'basic' : [
@@ -193,13 +193,15 @@ KIT_SPECS = {
         ( ['Heroine', 'Carrot'],   [1] ),   # rivers
         ( ['Tiara', 'Grimoire'],   [1] ),   # schala
         ( ['RubyRing', 'CharmHarp'],   [1] ),   # zoe
+        ( ['Sleep', 'Hermes'],     [1] ),   # wylem
+        ( ['Rune', 'Stardust'],    [1] ),   # leggy
     ],
 
     'atb' : [
-        ( ['SilkWeb'],  [(2,3)] ),
-        ( ['Hermes'],   [(4,5)] ),
-        ( ['HrGlass1'], [1] ),
-        ( ['Heal'],     [(3,4)] ),
+        ( 'SilkWeb',  [(2,3)] ),
+        ( 'Hermes',   [(4,5)] ),
+        ( 'HrGlass1', [1] ),
+        ( 'Heal',     [(3,4)] ),
     ],
 
     'adamant' : [
@@ -214,10 +216,232 @@ KIT_SPECS = {
         ('Exit', [(5,10)])
     ],
 
-    'hero' : None # special case handling
+    'hero' : None, # special case handling
+     
+    'egg' : None, # special case handling
 }
 
+EGG_METHODS = {
+    'coffin' : 
+        { 'freq' : 5, 
+          'users' : ['dkcecil', 'pcecil', 'kain', 'crydia', 'tellah', 'edward', 'rosa', 'yang', 'palom', 'porom', 'cid', 'edge', 'fusoya'],
+          'type' : 'item',
+          'items' : [
+              ('Siren', [1]),
+              ('Coffin', [1])
+              ] },
+    
+    'grimoire' : 
+        { 'freq' : 2,
+          'users' : ['dkcecil', 'pcecil', 'kain', 'crydia', 'tellah', 'edward', 'rosa', 'yang', 'palom', 'porom', 'cid', 'edge', 'fusoya'],
+          'type' : 'item',
+          'items' : [
+              ('Siren', [1]),
+              ('Grimoire', [1])
+              ] },
 
+    'black' : 
+        { 'freq' : 3,
+          'users' : ['dkcecil'],
+          'type' : 'fight',
+          'items' : [
+              ('Siren', [1]),
+              ('BlackSword', [1]), 
+              (['SilkWeb', 'HrGlass1'], [1]),
+              ] },
+
+    'medusa' : 
+        { 'freq' : 4,
+          'users' : ['pcecil', 'crydia', 'edward', 'rosa', 'palom', 'porom', 'cid', 'fusoya'],
+          'type' : 'fight',
+          'items' : [
+              ('Siren', [1]),
+              ('MedusaArrow', [1]),
+              ('Archer', [1]), 
+              (['SilkWeb', 'HrGlass1'], [1]),
+              ] },
+
+    'assassin' : 
+        { 'freq' : 4,
+          'users' : ['pcecil', 'kain', 'crydia', 'edward', 'palom', 'edge'],
+          'type' : 'fight',
+          'items' : [
+              ('Siren', [1]),
+              ('Assassin', [1]),
+              (['SilkWeb', 'HrGlass1'], [1]),
+              ] },
+
+    'ninjastar' : 
+        { 'freq' : 4,
+          'users' : ['edge'],
+          'type' : 'dart',
+          'items' : [
+              ('Siren', [1]),
+              ('NinjaStar', [1])
+              ] },
+
+    'recall' : 
+        { 'freq' : 2,
+          'users' : ['tellah'],
+          'type' : 'magic',
+          'items' : [
+              ('Siren', [1]),
+              ('Ether2', [2]),
+              ('PowerStaff', [1])
+              ] },
+
+    'baham' : 
+        { 'freq' : 2,
+          'users' : ['crydia'],
+          'type' : 'magic',
+          'items' : [
+              ('Siren', [1]),
+              ('Baham', [1]),
+              ('SomaDrop', [5]),
+              ('Change', [1])
+              ] },
+
+    'levia' : 
+        { 'freq' : 1,
+          'users' : ['crydia'],
+          'type' : 'magic',
+          'items' : [
+              ('Siren', [1]),
+              ('Levia', [1]),
+              ('SomaDrop', [4]),
+              ('ThunderRod', [1]),
+              ('Tiara', [1]),
+              ('Rune', [1])
+              ] },
+
+    'dragoon' : 
+        { 'freq' : 2,
+          'users' : ['kain'],
+          'type' : 'jump',
+          'items' : [
+              ('Siren', [1]),
+              ('DragoonSpear', [1]),
+              ('Headband', [1]),
+              ('Strength', [1])
+              ] },
+
+    'poisonaxe' : 
+        { 'freq' : 1,
+          'users' : ['pcecil', 'kain', 'cid'],
+          'type' : 'fight',
+          'items' : [
+              ('Siren', [1]),
+              ('SilkWeb', [1]),
+              ('StarVeil', [1]),
+              ('PoisonAxe', [1])
+              ] },
+
+    'venom' : 
+        { 'freq' : 1,
+          'users' : ['palom'],
+          'type' : 'magic',
+          'items' : [
+              ('Siren', [1]),
+              ('SilkWeb', [1]),
+              ('StarVeil', [1])
+              ] },
+
+    'dragonwhip' : 
+        { 'freq' : 3,
+          'users' : ['crydia'],
+          'type' : 'fight',
+          'items' : [
+              ('Siren', [1]),
+              ('DragonWhip', [1]),
+              ('CrystalRing', [1])
+              ] },
+
+    'heavyarmor' : # includes special processing to add a weapon
+        { 'freq' : 2,
+          'users' : ['pcecil', 'kain', 'cid'],
+          'type' : 'fight',
+          'items' : [
+              ('Siren', [1]),
+              ('Glass', [1]),
+              ('DragoonArmor', [1]),
+              ('Cursed', [1]) 
+              ] },
+
+    'sorcrobe' : # includes special processing to add a weapon
+        { 'freq' : 2,
+          'users' : ['crydia', 'tellah', 'rosa', 'palom', 'porom', 'fusoya'],
+          'type' : 'fight',
+          'items' : [
+              ('Siren', [1]),
+              ('Glass', [1]),
+              ('Sorcerer', [1]),
+              ('Cursed', [1])
+              ] },
+
+    'moonveil' : # includes special processing to add a weapon
+        { 'freq' : 2,
+          'users' : ['dkcecil', 'pcecil', 'kain', 'crydia', 'tellah', 'edward', 'rosa', 'yang', 'palom', 'porom', 'cid', 'edge', 'fusoya'],
+          'type' : 'fight',
+          'items' : [
+              ('Siren', [1]),
+              ('MoonVeil', [1]),
+              ('Cure2', [10])
+              ] },
+
+    'magicspear' : 
+        { 'freq' : 2,
+          'users' : ['kain'],
+          'type' : 'fight',
+          'items' : [
+              ('Siren', [1]),
+              (['FlameSpear', 'BlizzardSpear', 'WhiteSpear'], [1]),
+              ('StarVeil', [(2,3)])
+              ] },
+
+    'drain' : 
+        { 'freq' : 1,
+          'users' : ['pcecil', 'kain'],
+          'type' : 'fight',
+          'items' : [
+              ('Siren', [1]),
+              ('DrainSword', [1]),
+              ('Glass', [1]),
+              ('DragoonArmor', [1])
+              ] },
+
+    'artemis' : 
+        { 'freq' : 1,
+          'users' : ['pcecil', 'crydia', 'edward', 'rosa', 'palom', 'porom', 'cid', 'fusoya'],
+          'type' : 'fight',
+          'items' : [
+              ('Siren', [1]),
+              ('ArtemisArrow', [2]),
+              ('ArtemisBow', [1]),
+              ('NinjaHelm', [1])
+              ] },
+
+    'spoon' : 
+        { 'freq' : 1,
+          'users' : ['edward'],
+          'type' : 'fight',
+          'items' : [
+              ('Siren', [1]),
+              ('NinjaHelm', [1]),
+              ('PowerShirt', [1]),
+              ('CrystalRing', [1]),
+              ('Bacchus', [1])
+          ] },
+
+    'bear' :
+        { 'freq' : 1, 
+          'users' : ['yang'],
+          'type' : 'bear',
+          'items' : [
+              ('Siren', [1]),
+              ('PoisonClaw', [1]),
+              ('Cure2', [10])
+          ] },
+}
 
 def apply(env):
     kits = []
@@ -270,6 +494,77 @@ def apply(env):
                 quantity = [20] if (weapon1.subtype == 'bow' and ('unstackable' not in env.meta.get('wacky_challenge', []))) else [1]
                 kit_spec = kit_spec + [ ( [ weapon2 ], quantity ) ]
             kit_spec = kit_spec + [ ([ armor ], [1]), ( [ head ], [1]), ( [ hand ], [1]) ]
+        elif kit_name == 'egg':
+            char = env.meta['starting_character']
+            if char == 'cecil':
+                if env.options.flags.has('characters_cecil_paladin'):
+                    char = 'pcecil'
+                else:
+                    char = 'dkcecil'
+            if char == 'rydia':
+                char = 'crydia'
+            egg_kits = []
+            egg_weights = []
+            for ekit in EGG_METHODS:
+                to_add = False
+                wackies = env.meta.get('wacky_challenge', [])
+                starting_item = env.meta['rewards_assignment'][RewardSlot.starting_item].item
+                if EGG_METHODS[ekit]['type'] == 'item':
+                    to_add = True
+                elif (EGG_METHODS[ekit]['type'] == 'fight' and 'omnidextrous' in wackies
+                                                        and ekit in ['magicspear', 'poisonaxe']):
+                    to_add = True
+                elif (EGG_METHODS[ekit]['type'] == 'fight' and 'fistfight' in wackies):
+                    continue
+                elif (EGG_METHODS[ekit]['type'] == 'fight' and 'musical' in wackies and not env.options.flags.has('jump')
+                                                        and ekit not in ['magicspear', 'poisonaxe', 'spoon'] and char not in ['kain', 'rosa']):
+                    continue
+                elif EGG_METHODS[ekit]['type'] == 'fight' and 'darts' in wackies and not (env.options.flags.has('jump') or char == 'kain'):
+                    if ((ekit in ['dragoon', 'poisonaxe', 'magicspear'] and char in ['tellah', 'cid', 'edge', 'fusoya'])
+                        or (ekit in ['artemis'] and char in ['edge', 'fusoya'])):
+                        to_add = True
+                    else:
+                        continue
+                elif ((starting_item != '#item.Spoon') 
+                            or (not env.options.flags.has('edward_spoon') and not 'omnidextrous' in wackies)
+                            or (('menarepigs' in wackies and char not in ['crydia', 'rosa', 'porom']) and not env.options.flags.has('jump'))) and ekit == 'spoon':
+                    continue
+                elif EGG_METHODS[ekit]['type'] == 'fight' and 'omnidextrous' in wackies:
+                    to_add = True
+                elif EGG_METHODS[ekit]['type'] == 'jump' and 'omnidextrous' in wackies and env.options.flags.has('jump'):
+                    to_add = True
+                elif EGG_METHODS[ekit]['type'] == 'dart' and 'darts' in wackies and char in ['edge', 'fusoya']:
+                    to_add = True
+                elif not env.options.flags.has('japanese_abilities') and ekit in ['recall', 'bear']:
+                    continue
+                elif 'misspelled' in wackies and ekit in ['baham', 'levia', 'venom']:
+                    continue
+                elif 'unstackable' in wackies and ekit in ['baham', 'levia', 'bear', 'moonveil']:
+                    continue
+                elif char in EGG_METHODS[ekit]['users']:
+                    to_add = True
+                if to_add:
+                    egg_kits.append(ekit)
+                    egg_weights.append(EGG_METHODS[ekit]['freq'])
+            chosen_kit = env.rnd.choices(egg_kits, weights=egg_weights, k=1)[0]
+            kit_spec = EGG_METHODS[chosen_kit]['items']
+            if chosen_kit in ['heavyarmor', 'sorcrobe', 'moonveil']:
+                weapons_dbview = items_dbview.get_refined_view(lambda it: it.category == 'weapon' and it.subtype != 'arrow' and it.tier in (3,5) and char in it.equip)
+                if 'unstackable' in wackies:
+                    weapons_dbview.refine(lambda it: it.subtype != 'bow')
+                weapon1 = env.rnd.choice(weapons_dbview.find_all())
+                kit_spec.append( (weapon1.const[6:], [1]) )
+                if weapon1.subtype == 'bow':
+                    arrows_dbview = items_dbview.get_refined_view(lambda it: it.category == 'weapon' and it.subtype == 'arrow' and it.tier in (3,5) and char in it.equip)
+                    weapon2 = env.rnd.choice(arrows_dbview.find_all())
+                    kit_spec.append( (weapon2.const[6:], [30]) )
+            elif chosen_kit in ['baham', 'levia'] and 'tellahmaneuver' in wackies:
+                kit_spec[2] = ('AuApple', [(6 if chosen_kit == 'baham' else 5)])
+            elif chosen_kit in ['magicspear'] and 'darts' in wackies:
+                if char in ['tellah', 'cid']:
+                    kit_spec[1] = ('WhiteSpear', [1])
+                elif char == 'edge':
+                    kit_spec[1] = (['BlizzardSpear', 'WhiteSpear'], [1])
         elif kit_name == 'wacky_challenge':
             kit_spec = env.meta['wacky_starter_kit']
         elif kit_name == 'objective':
