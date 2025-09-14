71693459a5a189bf99eb52a4f08b0d64ea95318c
ScytheMarshall <172561988+ScytheMarshall@users.noreply.github.com>
2025-09-13 20:14:24 -0400

    Fixing Kmiab Pnone Tvanilla/etc generation issue
    On Tshuffle and Tvanilla with Pnone, Baron Castle does not give a
    key item reward, which means there is supposed to be an empty reward
    that gets shuffled into the pool. However, treasure_rando.py tries
    to write spoiler log data for an empty reward in a miab, which fails.
    
    While we could write special cases to put a Cure1 there, it is mostly
    easier to just remove the empty reward entirely and always just
    place a Cure1 where normally there would be nothing (which is the
    same behaviour, roughly, as when Tplayable cannot place items in
    miabs). So, that's the change in core_rando.py.

--- FreeEnt/core_rando.py
+++ FreeEnt/core_rando.py
@@ -980,7 +980,8 @@ def apply(env):
                         rewards_assignment[slot] = pool.pop()
                     except:
                         # Pnone + win:crystal causes an issue under Tvanilla | Tshuffle. This is a workaround to that.
-                        rewards_assignment[slot] = (default_item_reward if not is_vanilla else EmptyReward())
+                        # ... and, to prevent further issues, just always give the default reward, instead of nothing.
+                        rewards_assignment[slot] = default_item_reward
     else:
         # revised Rivers rando
         curves_dbview = databases.get_tvanillaish_dbview() if (env.options.flags.has('treasure_vanillaish')) else databases.get_curves_dbview()
