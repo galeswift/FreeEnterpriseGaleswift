b1ff80e1dffffb666b4cef548434b544cf6f24dc
ScytheMarshall <172561988+ScytheMarshall@users.noreply.github.com>
2026-08-24 07:43:47 -0400

    Update to v4.6.5.Gale
    Instead of a bugfix patch for v4.6.4, we're going to go to v4.6.5
    and do a better job of versioning going forward. In particular:
    
    Versioning guidelines:
    - keep MAJOR = 4, to distinguish from main branch v5.0
      (at least until we get there)
    - use MINOR for non-trivial changes, like flags/UI changes and
      significant behaviour
    - use PATCH for smaller/trivial changes, like bugfixes
    
    The only change here is in version.py.

--- FreeEnt/version.py
+++ FreeEnt/version.py
@@ -1,6 +1,6 @@
 MAJOR = 4
 MINOR = 6
-PATCH = 4
+PATCH = 5
 # The VERSION.STR cannot be more than 14 total characters long.
 # It gets pre-pended with "v" and postpended with "[$00]" for use in-game.
 # Beta versions tack on a ".b" at the end, so you only get 12 actual characters.
@@ -8,6 +8,10 @@ PATCH = 4
 #   f4c.ff4bin.rom.RomError: Patch at 10D810 conflicts with prior patch at 10D800
 # otherwise.
 FORK = "Gale"
+# Versioning guidelines:
+# - keep MAJOR = 4, to distinguish from main branch v5.0 (at least until we get there)
+# - use MINOR for non-trivial changes, like flags/UI changes and significant behaviour
+# - use PATCH for smaller/trivial changes, like bugfixes
 
 VERSION = (MAJOR, MINOR, PATCH, FORK)
 NUMERIC_VERSION = (MAJOR, MINOR, PATCH)
