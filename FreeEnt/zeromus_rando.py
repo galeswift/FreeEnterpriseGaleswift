6b2a135fca5fee49ec360fb899a29e4a6b722f3f
ScytheMarshall <172561988+ScytheMarshall@users.noreply.github.com>
2026-09-04 10:23:50 -0400

    Bugfix for Zunsure: and updating Z spoilers
    Firstly, the Zeromus script changes have been moved to their own
    section of the spoiler log. There you'll find the script category if
    it's non-vanilla (or vanilla if it was unclear what you'd get) as
    well as any changed moves or scripts, as usual.
    
    The main fix here is that Zunsure: was broken due to poor string
    management. When Zunsure: was on, you would always get the vanilla
    script category. That's been fixed!
    
    All changes are in zeromus_rando.py.

--- FreeEnt/zeromus_rando.py
+++ FreeEnt/zeromus_rando.py
@@ -229,7 +229,10 @@ def apply(env):
         script_category = main_z_flag[1:]
     else:
         # randomly choose from the Zunsure options or the main category, which flagsetcore ensures are disjoint
-        script_category = env.rnd.choice([main_z_flag] + z_unsure_flags)[1:]
+        script_category = env.rnd.choice([main_z_flag[1:]] + [f[8:] for f in z_unsure_flags])
+        if script_category == 'vanilla':
+            # in cases where the script category was not guaranteed and we get the vanilla script, spoil that
+            env.spoilers.add_table("ZEROMUS SCRIPT", [("Script category", "Vanilla")], public = env.options.flags.has_any('-spoil:all', '-spoil:misc'))
     
     bigbang_replacements = [[], [], [], [], [], []]
     script_nuke_replacement = []
@@ -389,13 +392,16 @@ def apply(env):
         # for reactions: first two are vanilla, third is custom react-to-Dart, fourth is custom react-to-Aim, fifth is react-to-Jump (Land), sixth is react-to-Fight
         # will need to change in the event of writing a compiler for new ai sets/etc.
         env.add_binary(address.UnheaderedAddress(0x0764A1), [0x61, 0x56, 0x18, 0x54, 0x05, 0x4E, 0x30, 0x58, 0x5C, 0x4F, 0x06, 0x4D]) 
+
+        # spoiler
+        env.spoilers.add_table("ZEROMUS SCRIPT", [("Script category", "Physical")], public = env.options.flags.has_any('-spoil:all', '-spoil:misc'))
     
     elif script_category == 'ailments':
         # the normal script, but the six different Nuke/Virus casts are replaced with status spells (let's make them all different, for variety)
         # no, we're not changing the Count cast in ET Meteo phase
         replacescriptflag = True
         potential_spells = list(POSSIBLE_STATUS_SPELLS)
-        ailments_spoilers = []
+        ailments_spoilers = [("Script category", "Ailments")]
         ailments_to_use = env.rnd.sample(potential_spells, k=6)
         targeting_data = [env.rnd.choice(POSSIBLE_STATUS_SPELLS[sp].split(' / ')) for sp in ailments_to_use]
         script_fragments = []
@@ -466,7 +472,7 @@ def apply(env):
         # add status protection, for added danger... to us
         env.add_substitution('zeromus status protection',
                              'resist status #Poison #Blind #Mute #Piggy #Mini #Toad #Stone #Swoon #Calcify1 #Calcify2 #Berserk #Charm #Sleep #Stun #Curse')
-        env.spoilers.add_table("MISC", ailments_spoilers, public = env.options.flags.has_any('-spoil:all', '-spoil:misc'))    
+        env.spoilers.add_table("ZEROMUS SCRIPT", ailments_spoilers, public = env.options.flags.has_any('-spoil:all', '-spoil:misc'))    
 
     elif script_category == 'chaos':
         # 2-5 random attacks per phase (or 1-3 for last phase), sometimes with a pass-shake-BB-type pattern (at most floor(n/2) of them), random reactions
@@ -477,7 +483,7 @@ def apply(env):
         potential_shake_attacks = list(shake_dict)
         potential_non_shake_attacks = list(POSSIBLE_CHAOS_COMMANDS)
         potential_reactions = list(POSSIBLE_CHAOS_REACTIONS)
-        chaos_spoilers = []
+        chaos_spoilers = [("Script category", "Chaos")]
         spoilers_to_add = [[], [], []]
 
         for i in range(0,3):
@@ -697,7 +703,7 @@ def apply(env):
         counter_virus_replacement.extend(reaction_list[3])
 
         env.add_file('scripts/zeromus_chaosscript.f4c')
-        env.spoilers.add_table("MISC", chaos_spoilers, public = env.options.flags.has_any('-spoil:all', '-spoil:misc'))        
+        env.spoilers.add_table("ZEROMUS SCRIPT", chaos_spoilers, public = env.options.flags.has_any('-spoil:all', '-spoil:misc'))        
 
     elif script_category == 'lavosshell':
         # assign three random scripts from the game, avoiding condition changes
@@ -768,8 +774,9 @@ def apply(env):
                     ])
         # spoiler log:
         env.spoilers.add_table(
-            "MISC", 
-            [ ("Zeromus script 1", script_descriptions[scripts[0]]),
+            "ZEROMUS SCRIPT", 
+            [ ("Script category", "Lavos Shell"),
+            ("Zeromus script 1", script_descriptions[scripts[0]]),
             ("Zeromus script 2", script_descriptions[scripts[1]]),
             ("Zeromus script 3", script_descriptions[scripts[2]]) ],
             public = env.options.flags.has_any('-spoil:all', '-spoil:misc')
@@ -862,7 +869,7 @@ def apply(env):
 
         # build spoiler log
         env.spoilers.add_table(
-            "MISC", 
+            "ZEROMUS SCRIPT", 
             [ ("Opening Big Bang replacement", databases.get_spell_spoiler_name(replacement_commands[0])),
             ("Virus phase Big Bang replacements", ', '.join([databases.get_spell_spoiler_name(replacement_commands[i]) for i in range(1,4)])),
             ("Nuke phase Big Bang replacements", ', '.join([databases.get_spell_spoiler_name(replacement_commands[i]) for i in range(4,6)])) ],
@@ -879,7 +886,7 @@ def apply(env):
         phases_name_dict = ( {0x4C : 'Needle', 0x55 : 'Jump', 0x57 : 'Dark Wave'} if physicalflag 
                             else {0x4C : 'Virus', 0x55 : 'Nuke', 0x57 : 'Meteo'} )
         env.spoilers.add_table(
-            "MISC", 
+            "ZEROMUS SCRIPT", 
             [ ("Zeromus phase order", ', '.join([phases_name_dict[p] for p in phases])) ],
             public = env.options.flags.has_any('-spoil:all', '-spoil:misc')
             )        
